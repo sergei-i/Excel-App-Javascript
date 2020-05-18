2cf328dedf4d253a25781dde8392699366cbb519
Sergei Iakovlev <1161441@gmail.com>
2020-05-18 12:55:25 +0200

    Finish base table logic (#5)
    * Finish base table logic
    
    * Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,17 +4,39 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
+
+    this.prepare()
   }
 
+  // Настраивает компонент до init
+  prepare() {}
+
+  // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
 
+  // Уведомляет слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписывает на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // Инициализирует компонент, добавляет DOM слушателей
   init() {
     this.initDOMListeners()
   }
 
+  // Удаляет компонент, чистит слушателей
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
